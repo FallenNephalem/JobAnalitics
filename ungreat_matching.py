73bd3bcc4f62f222e2fd2136f55ea1dd753e0e1c
FallenNephalem <ldr200@mail.ru>
2020-01-25 19:23:55 +0300

    num_match

--- ungreat_matching.py
+++ ungreat_matching.py
@@ -1,6 +1,14 @@
 import math
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
+#from fuzzywuzzy import fuzz
+#from fuzzywuzzy import process
+
+
+def num_match(num1, num2):
+	if num1>num2:
+		return num1
+	else:
+		return num2
+
 def ungreat_match(str1,str2):
 	i=0
 	lenth1 = len(str1)
@@ -12,14 +20,17 @@ def ungreat_match(str1,str2):
 		return False
 	else:
 		for char in str1:
+			if i<(num_match(lenth1, lenth2)-1):
 
-			if char == str2[i]:
-				print('true')
-				mk+=1
-			else:
-				print('fail')
+				if char == str2[i]:
+					#print('true')
+					mk+=1
+				#else:
+					#print('fail')
 			i+=1
-		if mk < (lenth1-3) & (mk < lenth2-3):
+		print(mk)
+
+		if mk==num_match(lenth1, lenth2):
 			return True
 		else:
 			return False
