40122545fb570bd654de36adfdfc35d6b070335e
FallenNephalem <ldr200@mail.ru>
2020-02-01 10:26:46 +0300

    initial debug

--- ungreat_matching.py
+++ ungreat_matching.py
@@ -1,6 +1,12 @@
 import math
 #from fuzzywuzzy import fuzz
 #from fuzzywuzzy import process
+alfabet = [['q','Q'],['w','W'],['e','E'],['r','R'],
+['t','T'],['y','Y'],['u','U'],['i','I'],['o','O'],
+['p','P'],['a','A'],['s','S'],['d','D'],['f','F'],
+['g''G'],['h','H'],['j','J'],['k','K'],['l','L'],
+['z','Z'],['x','X'],['c','C'],['v','V'],['b','B'],
+['n','N'],['m','M']]
 
 
 def num_match_max(num1, num2):
@@ -15,6 +21,20 @@ def num_match_min(num1, num2):
 	else:
 		return num1
 
+def one_register(str1):
+	i=0
+	while i<len(str1):
+		j=0
+		while j<26:
+			print(alfabet[15][1])
+			if str1[i] == alfabet[j][1]:
+				str1[i] = alfabet[j][0]
+			j+=1
+			print("j"+str(j))
+		i+=1
+		print("i"+str(i))
+
+
 def ungreat_match(str1,str2):
 	i=0
 	lenth1 = len(str1)
@@ -22,6 +42,8 @@ def ungreat_match(str1,str2):
 	#print(str2)
 	mk = 0
 	min_num = num_match_min(lenth1, lenth2)
+	str1 = one_register(str1)
+	str2 = one_register(str2)
 	if str1 == str2:
 		keys = [str1, 'True']
 		return keys
