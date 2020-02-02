07c6d368b60cb41870ec29e7dbc124cce66921a0
FallenNephalem <ldr200@mail.ru>
2020-02-02 10:59:01 +0300

    to fix

---
+++ readmebeforestart.txt
@@ -0,0 +1,1 @@
+Bug was in place, where you try to change value of element in str1.
--- ungreat_matching.py
+++ ungreat_matching.py
@@ -4,7 +4,7 @@ import math
 alfabet = [['q','Q'],['w','W'],['e','E'],['r','R'],
 ['t','T'],['y','Y'],['u','U'],['i','I'],['o','O'],
 ['p','P'],['a','A'],['s','S'],['d','D'],['f','F'],
-['g''G'],['h','H'],['j','J'],['k','K'],['l','L'],
+['g','G'],['h','H'],['j','J'],['k','K'],['l','L'],
 ['z','Z'],['x','X'],['c','C'],['v','V'],['b','B'],
 ['n','N'],['m','M']]
 
@@ -26,7 +26,7 @@ def one_register(str1):
 	while i<len(str1):
 		j=0
 		while j<26:
-			print(alfabet[15][1])
+			print(alfabet[j][1])
 			if str1[i] == alfabet[j][1]:
 				str1[i] = alfabet[j][0]
 			j+=1
