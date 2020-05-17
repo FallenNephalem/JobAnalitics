5268c13d7e814c167dca9f07d45458845dfc0bec
FallNeph <lyubashevskiy_dr_0818@1511.ru>
2020-05-17 20:37:23 +0300

    alfa version

--- jobanalitics.py
+++ jobanalitics.py
@@ -4,7 +4,8 @@ from ungreat_matching import ungreat_match
 
 headers = {'accept': '*/*',
            'user-agent': 'Chrome/78.0.3904.108'}
-first_url = 'https://hh.ru/search/vacancy?area=1&st=searchVacancy&text=python'
+first_url = 'https://hh.ru/search/vacancy?area=1&st=searchVacancy&text=junior+python'
+
 links = []
 skills = []
 
@@ -13,21 +14,40 @@ session = requests.Session()
 request = session.get(first_url, headers=headers)
 if request.status_code == 200:
     soup = bs(request.content,'html.parser')
-    divs = soup.find_all('div', attrs={'data-qa':'vacancy-serp__vacancy'}) #vacancy-serp__vacancy_premium
-    for div in divs:
-        title = div.find('a', attrs={'data-qa':'vacancy-serp__vacancy-title'})['href']
-        #print(title)
-        links.append(title)
-    print('wait a minete...')
-    for url in links:
-        session = requests.Session()
-        request = session.get(url, headers=headers)
-        if request.status_code == 200:
-            soup = bs(request.content,'html.parser')
-            divs = soup.find_all('div', attrs={'data-qa':'bloko-tag bloko-tag_inline skills-element'})
-            for div in divs:
-                skills.append(div.find('span', attrs={'data-qa':'bloko-tag__text'}).text)
-#print(skills)
+    first_num = int(soup.find('a',attrs={'class':'bloko-button HH-Pager-Control'}).text)
+    containers = soup.find_all('span', attrs={'class':'pager-item-not-in-short-range'})
+    for container in containers:
+        num_of_pages = int(container.find('a',attrs={'class':'bloko-button HH-Pager-Control'}).text)
+    try:
+        print(num_of_pages)
+    except NameError:
+        print(first_num)
+        num_of_pages = first_num
+    except NameError:
+        num_of_pages = 1
+
+
+session = requests.Session()
+for i in range(num_of_pages):
+    link = 'https://hh.ru/search/vacancy?L_is_autosearch=false&area=1&clusters=true&enable_snippets=true&text=python&page={0}'.format(i)
+    request = session.get(first_url, headers=headers)
+    if request.status_code == 200:
+        soup = bs(request.content,'html.parser')
+        divs = soup.find_all('div', attrs={'data-qa':'vacancy-serp__vacancy'}) #vacancy-serp__vacancy_premium
+        for div in divs:
+            title = div.find('a', attrs={'data-qa':'vacancy-serp__vacancy-title'})['href']
+            #print(title)
+            links.append(title)
+        print('wait a minete...')
+        for url in links:
+            session = requests.Session()
+            request = session.get(url, headers=headers)
+            if request.status_code == 200:
+                soup = bs(request.content,'html.parser')
+                divs = soup.find_all('div', attrs={'data-qa':'bloko-tag bloko-tag_inline skills-element'})
+                for div in divs:
+                    skills.append(div.find('span', attrs={'data-qa':'bloko-tag__text'}).text)
+print(skills)
 
 
 i=0
@@ -36,7 +56,7 @@ while i<len(skills):
     result = ungreat_match(skills[0],skills[i])
     if result[1] == 'True':
         skill_is_exist = statistic.get(result[0])
-        print(result[0], skill_is_exist)
+        #print(result[0], skill_is_exist)
         if skill_is_exist == None:
             statistic[result[0]] = 1
         else:
